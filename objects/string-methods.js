90c65291a6f5983b990b0e3694779626a69c1a27
Daniel Jones <daniel.jones.94@cantab.net>
2019-01-21 21:29:34 +0000

    Update string-methods.js

--- objects/string-methods.js
+++ objects/string-methods.js
@@ -15,3 +15,13 @@ console.log(name.toLowerCase().includes('dan'))
 
 //trim
 console.log(name.trim())
+
+//challenge, is valid password, return true if valid legnth > 8 and no word password
+
+let validPassword = function(password) {
+    return password.length > 8 && !password.includes('password')
+}
+
+console.log(validPassword('123w'))
+console.log(validPassword('mypassword'))
+console.log(validPassword('sdsadsa2wesad'))
