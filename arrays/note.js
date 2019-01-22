e040d0337771a3c4f7dbbea4328c78589fa64b9b
Daniel Jones <daniel.jones.94@cantab.net>
2019-01-22 21:39:09 +0000

    more

--- arrays/note.js
+++ arrays/note.js
@@ -6,8 +6,13 @@ notes.push('MyNote3') // adds an item to the end of array
 notes.shift() // remove first item
 notes.unshift('MyNote1')
 
-notes.splice(1, 1) // removes one item from position 1
+notes.splice(1, 1, 'MyNote2','MyNoteExtra') // removes one item from position 1
 
-console.log(notes.length)
-console.log(notes[0])
+console.log(notes)
+
+notes[2] = 'This is the new extra note' // replaces item at that position
+
+console.log(notes.length) // length of array
+console.log(notes)
+console.log(notes[0])// first item
 console.log(notes[notes.length-1]) // item in array at position 0
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,5 +1,13 @@
 const todo = ['Todo1', 'Todo2', 'Todo3', 'Todo4', 'Todo5']
 
+todo.splice(2 ,1)
+console.log(todo)
+todo.push('New to do')
+console.log(todo)
+todo.shift()
+console.log(todo)
+
+
 console.log(`You have ${todo.length} todos`)
 console.log(todo[0])
 console.log(todo[todo.length-2])
