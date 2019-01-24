c47a79d9d60fd07a7deb14d08d256d29cc3f063b
Daniel Jones <daniel.jones.94@cantab.net>
2019-01-24 23:02:31 +0000

    Update note.js

--- arrays/note.js
+++ arrays/note.js
@@ -1,20 +1,29 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{
+    title: 'Note 1', 
+    body: 'body1'
+}, {
+    title: 'Note 2', 
+    body: 'body2',
+}, {
+    title: 'Note3',
+    body: 'body3'
+}]
 
-notes.pop() // removes the last item in the array
-notes.push('MyNote3') // adds an item to the end of array
+// notes.pop() // removes the last item in the array
+// notes.push('MyNote3') // adds an item to the end of array
 
-notes.shift() // remove first item
-notes.unshift('MyNote1')
+// notes.shift() // remove first item
+// notes.unshift('MyNote1')
 
-notes.splice(1, 1, 'MyNote2','MyNoteExtra') // removes one item from position 1
+// notes.splice(1, 1, 'MyNote2','MyNoteExtra') // removes one item from position 1
 
-console.log(notes)
+// console.log(notes)
 
-notes[2] = 'This is the new extra note' // replaces item at that position
+// notes[2] = 'This is the new extra note' // replaces item at that position
 
-notes.forEach(function (item, index) { // loops each item in the array and we are given arguments of the item in the array and the index of the item
-    console.log(`${item} ${index}`)
-})
+// notes.forEach(function (item, index) { // loops each item in the array and we are given arguments of the item in the array and the index of the item
+//     console.log(`${item} ${index}`)
+// })
 
 console.log(notes.length) // length of array
 console.log(notes)
@@ -29,3 +38,11 @@ for (let count = 0; count <= 2; count++) {
 for (let count = notes.length - 1 ; count >= 0; count--) {
     console.log(notes[count])
 }
+
+console.log(notes.indexOf('MyNote1'))
+
+const index = notes.findIndex(function (note, index) {
+    return note.title === 'Note 2'
+})
+
+console.log(index)
