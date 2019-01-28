cc0163e4ce7fd5e1b143d621a044bab81a4864e1
Daniel Jones <daniel.jones.94@cantab.net>
2019-01-28 22:40:59 +0000

    More dom

---
+++ todo-app/todo-app.js
@@ -0,0 +1,32 @@
+// const ps = document.querySelectorAll('p')
+
+// ps.forEach(function (item, index) {
+//     // console.log(index)
+//     if (item.textContent.includes('the')){
+//         item.remove()
+//     }
+// })
+
+const todo = ['Clean the car', 'Was the dishes', 'Take out rubbish', 'Finish course', 'Clean floor']
+
+todo.forEach(function (item, index) {
+    todo.splice(index, 1, {text: item, completed: (Math.random() < 0.5)})
+})
+// you have x todos left
+// add a p value
+
+let remainingCount = 0
+todo.forEach(function (item, index) {
+    if (!item.completed) {
+        remainingCount += 1
+        const remainingTodo = document.createElement('p')
+        remainingTodo.textContent = item.text
+        document.querySelector('body').appendChild(remainingTodo)
+    }
+})
+
+const remaining = document.createElement('h4')
+remaining.textContent = `You have ${remainingCount} todo's remaining`
+document.querySelector('body').appendChild(remaining)
+
+
