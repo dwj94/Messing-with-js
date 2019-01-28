fb2b5cf50b1ef2ff591f25b2fdae25101432d953
Daniel Jones <daniel.jones.94@cantab.net>
2019-01-28 22:37:20 +0000

    More dom manip

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -2,10 +2,23 @@
 
 // const p = document.querySelector('p')
 // p.remove()
-
+const notes = [{
+    title: 'Bote 1', 
+    body: 'body1'
+}, {
+    title: 'Aote 2', 
+    body: 'body2',
+}, {
+    title: 'aote3',
+    body: 'body3'
+}]
 
 const ps = document.querySelectorAll('p')
 ps.forEach(function (item, index){
     item.textContent = '**'
     // item.remove()
 })
+
+const newPar = document.createElement('p')
+newPar.textContent = 'This is a new element from JS'
+document.querySelector('body').appendChild(newPar)
