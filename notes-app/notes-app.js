f326ca2eb8745ec4a6fdfe4595ecb7e2b01cccfc
Daniel Jones <daniel.jones.94@cantab.net>
2019-02-14 18:50:08 +0000

    refactor

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,38 +1,7 @@
-
-let notes = []
-
-// Check for existing saved data
-const notesJSON = localStorage.getItem('notes')
-
-if (notesJSON !== null) {
-    notes = JSON.parse(notesJSON)
-}
-
+const notes = getSavedNotes()
 
 const filters = {
     searchText: ''
-}
-
-
-const renderNotes = function (notes, filters) {
-    const filteredNotes = notes.filter(function (note) {
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-    
-    if (notes.length > 0) {
-        document.querySelector('#notes').innerHTML = '' 
-    }
-
-    filteredNotes.forEach(function (item, index) {
-        const newNote = document.createElement('p')
-
-        if (item.title.length > 0) {
-            newNote.textContent = item.title
-        } else {
-            newNote.textContent = 'Unnamed note'
-        }
-        document.querySelector('#notes').appendChild(newNote)
-    })
 }
 
 renderNotes(notes, filters)
---
+++ notes-app/notes-functions.js
@@ -0,0 +1,41 @@
+// read existing notes from local storage
+const getSavedNotes = function () {
+    // Check for existing saved data
+    const notesJSON = localStorage.getItem('notes')
+
+    if (notesJSON !== null) {
+        return JSON.parse(notesJSON)
+    } else {
+        return []
+    }
+}
+
+
+// generate the dom structure for a note
+
+const generateNoteDom = function (note) {
+    const newNote = document.createElement('p')
+
+    if (note.title.length > 0) {
+        newNote.textContent = note.title
+    } else {
+        newNote.textContent = 'Unnamed note'
+    }
+    return newNote
+}
+
+// render application notes
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    
+    if (notes.length > 0) {
+        document.querySelector('#notes').innerHTML = '' 
+    }
+
+    filteredNotes.forEach(function (item, index) {
+        const newNote = generateNoteDom(item)
+        document.querySelector('#notes').appendChild(newNote)
+    })
+}
